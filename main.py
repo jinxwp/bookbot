00be0b1572c5492b2de61795adee846dc555eb10
Jinxwp <mikemahnic@gmail.com>
2025-07-05 13:05:40 -0400

    first report steps completed

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from stats import number_of_words
+from stats import number_of_characters
+from stats import sorted_dictionary
 
 def main():
     path_to_file = "books/frankenstein.txt"
     text_from_book = get_book_text(path_to_file)
     numbers = number_of_words(text_from_book)
-    print(f"{numbers} words found in the document")    
+    characters = number_of_characters(text_from_book)
+    sorted_chars = sorted_dictionary(characters)
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_file}...")
+    print("----------- Word Count ----------")
+    print(f"Found {numbers} total words")
+    print("--------- Character Count -------")
+    for item in sorted_chars:
+        if item["char"].isalpha():
+            print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
--- stats.py
+++ stats.py
@@ -1,2 +1,20 @@
 def number_of_words(text_from_book):
     return len(text_from_book.split())
+
+def number_of_characters(text_from_book):
+    characters = {}
+    contents = text_from_book.lower()
+    for i in contents:
+        if i not in characters:
+            characters[i] = 1 
+        else:
+            characters[i] += 1
+    return characters
+
+def sorted_dictionary(characters):
+    charnum = []
+    for char in characters:
+        num = characters[char]
+        charnum.append({"char": char,"num": num})
+    charnum.sort(reverse=True, key=lambda n: n["num"])
+    return charnum
