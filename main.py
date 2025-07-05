3ab061325ed712fc6e2fe96dc604eff065492eae
Jinxwp <mikemahnic@gmail.com>
2025-07-05 13:56:25 -0400

    the end of the project

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from stats import number_of_words
 from stats import number_of_characters
 from stats import sorted_dictionary
+import sys
 
 def main():
-    path_to_file = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    path_to_file = sys.argv[1]
     text_from_book = get_book_text(path_to_file)
     numbers = number_of_words(text_from_book)
     characters = number_of_characters(text_from_book)
     sorted_chars = sorted_dictionary(characters)
+    
     
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {path_to_file}...")
